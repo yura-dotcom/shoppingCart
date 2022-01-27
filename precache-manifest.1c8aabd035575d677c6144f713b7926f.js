2ce2811d15eebfa5ab5858e40355274b3072efaf
master <devgeorg222@gmail.com>
2022-01-27 11:01:41 +0200

    Updates

---
+++ precache-manifest.1c8aabd035575d677c6144f713b7926f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f6e9a669e9d0cdc3f67be67ac3e5b2",
+    "url": "/shoppingCart/index.html"
+  },
+  {
+    "revision": "4f6e3d6aff35c545754f",
+    "url": "/shoppingCart/static/css/main.6bc763c2.chunk.css"
+  },
+  {
+    "revision": "0acda3645169aebccc17",
+    "url": "/shoppingCart/static/js/2.07f49bbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shoppingCart/static/js/2.07f49bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6e3d6aff35c545754f",
+    "url": "/shoppingCart/static/js/main.70a79257.chunk.js"
+  },
+  {
+    "revision": "5733e14f7098272f7ff9",
+    "url": "/shoppingCart/static/js/runtime-main.8b307f2b.js"
+  },
+  {
+    "revision": "cbe3a597681ba21fe242585dd5495cb3",
+    "url": "/shoppingCart/static/media/supermarket.cbe3a597.svg"
+  }
+]);
